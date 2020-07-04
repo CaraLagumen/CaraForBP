9f51e1e97d2b512a6f74bb269bcbbb41a1bc9715
Cara Lagumen <CaraLagumen@gmail.com>
2020-07-03 18:16:54 -0700

    Initial commit

---
+++ API/Migrations/20200704010450_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PMId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    CardOwnerStreet = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    CardOwnerZip = table.Column<string>(type: "varchar(9)", nullable: false),
+                    CardNumber = table.Column<string>(type: "varchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "varchar(5)", nullable: false),
+                    CVV = table.Column<string>(type: "varchar(3)", nullable: false),
+                    TransactionAmount = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PMId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ API/Models/PaymentDetail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(50)")]
+        [RegularExpression(@"[a-zA-Z \-']{1,50}",
+         ErrorMessage = "Only upper/lower alpha characters, spaces, hyphens, or apostrophes are allowed.")]
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [RegularExpression(@"[a-zA-Z0-9 ']{1,100}",
+         ErrorMessage = "Only upper/lower alpha characters or numbers are allowed.")]
+        public string CardOwnerStreet { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(9)")]
+        [RegularExpression(@"([0-9]{5}(-[0-9]{4})?)",
+         ErrorMessage = "Only numbers are allowed.")]
+        public string CardOwnerZip { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        [RegularExpression(@"[0-9]{13,16}",
+         ErrorMessage = "Only numbers are allowed.")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        [RegularExpression(@"[0-9]{2}\/[0-9]{2}",
+         ErrorMessage = "Please enter in MM/YY.")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        [RegularExpression(@"[0-9]{3}",
+         ErrorMessage = "Only numbers are allowed.")]
+        public string CVV { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [RegularExpression(@"([0-9]+)\.?[0-9][0-9]",
+         ErrorMessage = "Only numbers are allowed.")]
+        public string TransactionAmount { get; set; }
+    }
+}
